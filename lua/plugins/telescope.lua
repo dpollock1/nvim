48a965d285f412bfe31af7bfd43730338dcc2aa7
duncan <duncanjohnpollock@gmail.com>
2025-05-29 20:32:20 +0100

    added lsp ref keybinds to tele config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,6 +5,9 @@ return {
         config = function()
             local builtin = require("telescope.builtin")
             vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+            vim.keymap.set('n', '<C-g>', builtin.git_files, {})
+            vim.keymap.set('n', '<leader>gr', builtin.lsp_references, {})
+            vim.keymap.set('n', '<leader>gd', builtin.lsp_definitions, {})
             vim.keymap.set('n', '<Space><Space>', builtin.buffers, { noremap = true, silent = true, desc = "Buffers"})
             vim.keymap.set('n', '<C-f>', builtin.live_grep, { noremap = true, silent = true })
         end
