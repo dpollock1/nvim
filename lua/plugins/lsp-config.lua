2f7eaa2309734d7fb8d432bb4ed476073241e6ec
duncan <duncanjohnpollock@gmail.com>
2025-05-29 20:31:25 +0100

    changed completions

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -18,16 +18,24 @@ return {
             local lspconfig = require("lspconfig")
 
             lspconfig.lua_ls.setup({})
-            lspconfig.tsserver.setup({
+            lspconfig.ts_ls.setup({
              filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
             })
 
-            lspconfig.tsserver.setup({
+            lspconfig.ts_ls.setup({
                 filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
                 cmd = { "/run/current-system/sw/bin/typescript-language-server", "--stdio" },
                 root_dir = lspconfig.util.root_pattern("tsconfig.json", "jsconfig.json", ".git"), -- root detection
                 on_attach = function(client)
                  client.server_capabilities.documentFormattingProvider = false -- disable formatting by tsserver, use prettier instead
+                end,
+            })
+
+         -- Pyright LSP setup for Python
+            lspconfig.pyright.setup({
+                on_attach = function(client)
+                    -- You can customize further here for Pyright, e.g., for formatting or linting
+                    client.server_capabilities.documentFormattingProvider = true
                 end,
             })
 
